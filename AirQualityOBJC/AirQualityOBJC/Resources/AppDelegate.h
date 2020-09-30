8be2c612f7ede60348ad33e98e377a0caccfb12d
Trevor Bursach <bursacht@gmail.com>
2020-09-30 13:54:00 -0600

    New Starter

---
+++ AirQualityOBJC/AirQualityOBJC/Resources/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  AirQualityOBJC
+//
+//  Created by Trevor Bursach on 9/30/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ AirQualityOBJC/AirQualityOBJC/Resources/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AirQualityOBJC
+//
+//  Created by Trevor Bursach on 9/30/20.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
